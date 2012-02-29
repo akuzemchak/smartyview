650fb40918de7b32f246fc6aef03989a53a0763d
Korri <korrigan33@gmail.com>
2012-02-29 11:08:06 -0500

    More CodeIgniter styled config file

--- config/smartyview.php
+++ config/smartyview.php
@@ -2,10 +2,18 @@
 
 // config for smarty template library
 // more info about these settings at http://www.smarty.net/docs/en/installing.smarty.basic.tpl
-$config = array(
-	'smarty_template_dir' => APPPATH.'views', // base folder for all your templates
-	'smarty_compile_dir' => APPPATH.'cache/smarty/templates_c', // where templates will be compiled to
-	'smarty_config_dir' => APPPATH.'views/_config', // place to hold template config files
-	'smarty_cache_dir' => APPPATH.'cache/smarty', // universal smarty cache directory
-	'smarty_plugin_dir' => APPPATH.'helpers' //Default plugin directory to "application/helpers/"
-);
+
+//Base folder for all your templates
+$config['smarty_template_dir'] = APPPATH.'views';
+
+//Where templates will be compiled to
+$config['smarty_compile_dir'] = APPPATH.'cache/smarty/templates_c';
+
+//Place to hold template config files
+$config['smarty_config_dir'] = APPPATH.'views/_config';
+
+//Universal smarty cache directory
+$config['smarty_cache_dir'] = APPPATH.'cache/smarty';
+
+//Default plugin directory to "application/helpers/"
+$config['smarty_plugin_dir'] = APPPATH.'helpers';
